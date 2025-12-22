0eee9f6e72d1aca1cc4f31a9fabbd8965d7e3530
Luan <luan0319silva@gmail.com>
2025-12-22 12:16:30 -0300

    First Commit

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+ const btnMobile = document.querySelector('#mobile-btn');
+    const menu = document.querySelector('#menu');
+
+    btnMobile.addEventListener('click', () => {
+        menu.classList.toggle('show');
+        
+        btnMobile.textContent = menu.classList.contains('show') ? "✖" : "☰";
+
+        if (menu.classList.contains("show")) {
+            document.body.classList.add("no-scroll");
+        } else {
+            document.body.classList.remove("no-scroll");
+    }
+});
+
+menu.addEventListener('click', () => {
+     btnMobile.textContent = menu.classList.contains('show') ? "☰" : " ✖";
+    menu.classList.remove('show')
+
+    if (menu.classList.contains("show")) {
+            document.body.classList.add("no-scroll");
+        } else {
+            document.body.classList.remove("no-scroll");
+    }
+})
+
+
+
+
+const enviarWhats = (event) => {
+    event.preventDefault()  
+
+    const inputNome = document.querySelector('#nome').value;
+    const mensagem = document.querySelector('#mensagem').value;
+
+    if(inputNome && mensagem) {
+        let telefone = '5511964261935';
+    let text = `Olá! Me chamo ${inputNome}, ${ mensagem}`
+
+    let msgFormatada = encodeURIComponent(text)
+
+    let url = `https://wa.me/${telefone}?text=${msgFormatada}`;
+
+    window.open(url, '_blank');
+    } else {
+        alert('Preencha os dois campos')
+    }
+};
+
+
+
+
